454cbc84e0e72608a3fba5a10f90674c8482919f
June Hyok Cho <jcho0627@gmail.com>
2019-03-27 14:35:33 +0100

    selector exercise complete

---
+++ app.js
@@ -0,0 +1,8 @@
+// Select divs and change background color to purple
+$("div").css("backgroundColor", "purple")
+// Select divs with the class highlight and change the width to 200px
+$(".highlight").css("width", "200px")
+// Select the div with the id third and change the border to orange
+$("#third").css("border", "2px solid orange")
+// Select only the first div and chagne the font color to pink
+$("div:first-of-type").css("color", "pink")
